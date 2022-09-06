90069dddef422db0022b66c914c3a78716b9e61e
Kateryna Kasian <kkasyan1@gmail.com>
2022-09-06 18:01:14 +0300

    made task 2+3

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listEl = document.querySelector("#ingredients");
+
+const itemsEl = ingredients.map((item) => {
+  const itemsListEl = document.createElement("li");
+  itemsListEl.classList.add("item");
+  itemsListEl.textContent = item;
+  listEl.append(itemsListEl);
+
+  return itemsListEl;
+});
+
+listEl.append(...itemsEl);
